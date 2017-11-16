a255e3bb51ae0bb7e8ed3c17466e3aa5d12826ce
Neil Mizzi <b42040@mcast.org.mt>
2017-11-16 12:28:55 +0100

    Ball locket to paddle. Upon button click the ball is forced upwards Created borders for the game space Created brick prefabs and set a counter to calcuate amount of hits

---
+++ Brick Breaker 4.2A/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+    Vector3 ballPaddleDiff;
+
+    bool gameStarted = false;
+	// Use this for initialization
+	void Start () {
+        ballPaddleDiff = this.transform.position - paddle.transform.position;   
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!gameStarted)//if false
+        {
+            this.transform.position = paddle.transform.position + ballPaddleDiff;
+        }
+        if (Input.GetMouseButtonDown(0) && !gameStarted)
+        {
+            gameStarted= true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f,10f);
+        }
+	}
+}
---
+++ Brick Breaker 4.2A/Assets/Scripts/LoseBorder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseBorder : MonoBehaviour {
+
+    LevelManager levelManager = new LevelManager(); // created an instance/copy of Level manager
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger");
+        levelManager.LoadNewScene("Lose");
+    }
+
+    
+}
